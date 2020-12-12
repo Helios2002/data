09fd3bcccda7b8dace17bb32d19494053957bac3
Helios2002 <75824078+Helios2002@users.noreply.github.com>
2020-12-12 12:18:38 +0800

    Update strftime.py

--- strftime.py
+++ strftime.py
@@ -1,3 +1,6 @@
 import datetime
 ds = datetime.datetime.strptime('2020-09-15 14:09:44.0', "%Y-%m-%d %H:%M:%S.0")
 print(ds.date)
+class Weapon:
+    def __init__(self, energy):
+        self.energy = energy
